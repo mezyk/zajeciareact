f9d585ea2bb0d18234cb2016954e5d8e454b4864
mezyk <maaciauke@gmail.com>
2023-09-19 18:23:08 +0200

    v1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Header } from './components/header/Header';
+import { Footer } from './components/footer/Footer';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
