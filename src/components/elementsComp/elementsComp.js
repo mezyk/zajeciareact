8a84a82508912ec8faa5739dd2cda1de6991ea44
mezyk <maaciauke@gmail.com>
2023-11-03 21:06:53 +0100

    zadanie

--- src/components/elementsComp/elementsComp.js
+++ src/components/elementsComp/elementsComp.js
@@ -1,11 +1,42 @@
-export const ElementsComp = ({listItems}) => {
-    const handleClick = (item) => {
-        console.log(item); 
-      };
-    return (
-      <>
-        {listItems.map((item, index) => <li onClick={() => handleClick(item)} key={`listElement-${index}`} >{item}</li>)}
-      </>
-    );
-  }
+import React, { useState, useEffect } from 'react';
 
+export function ElementsComp() {
+  const [data, setData] = useState([]);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(data => {
+        setData(data);
+      });
+  }, []);
+
+  const handleItemClick = (item) => {
+    if (selectedItem === item) {
+      setSelectedItem(null);
+      document.title = 'Zadanie';
+    } else {
+      setSelectedItem(item);
+      document.title = item.title;
+    }
+  };
+
+  return (
+    <div>
+      <ul>
+        {data.map(item => (
+          <li
+            key={item.id}
+            onClick={() => handleItemClick(item)}
+            style={{
+              backgroundColor: selectedItem === item ? 'green' : 'transparent'
+            }}
+          >
+            {item.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
